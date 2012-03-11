b02c62fb1cd6f913009a5546fe312f2ffb15244e
Robin Morawetz <robin.morawetz@googlemail.com>
2012-03-11 15:59:21 +0100

    Remove unused shortcut "Navbar"

--- src/com/roman/romcontrol/CreateShortcut.java
+++ src/com/roman/romcontrol/CreateShortcut.java
@@ -65,8 +65,6 @@ public class CreateShortcut extends LauncherActivity {
             return R.drawable.ic_rom_control_led;
         else if (c.equals("Lockscreens"))
             return R.drawable.ic_rom_control_lockscreens;
-        else if (c.equals("Navbar"))
-            return R.drawable.ic_rom_control_navigation_bar;
         else if (c.equals("PowerMenu"))
             return R.drawable.ic_rom_control_power_menu;
         else if (c.equals("StatusBarBattery"))
